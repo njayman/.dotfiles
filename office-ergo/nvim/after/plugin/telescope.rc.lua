9d84bc2dff569a810524ca6e76c431ac7454dfbe
Najish Mahmud <najish@mirinfosys.com>
2022-12-09 15:06:35 +0600

    added telescope file manager

--- office-ergo/nvim/after/plugin/telescope.rc.lua
+++ office-ergo/nvim/after/plugin/telescope.rc.lua
@@ -8,7 +8,17 @@ local builtin = require("telescope.builtin")
 local map = vim.keymap.set
 
 telescope.setup({
+	defaults = {
+		file_ignore_patterns = {
+			"build",
+			"dist",
+			"node_modules",
+		},
+	},
 	extensions = {
+		file_browser = {
+			hijack_netrw = true,
+		},
 		fzf = {
 			fuzzy = true, -- false will only do exact matching
 			override_generic_sorter = true, -- override the generic sorter
@@ -22,17 +32,22 @@ telescope.setup({
 map("n", "<leader>fg", function()
 	builtin.live_grep()
 end, { noremap = true })
+
 map("n", "<leader>ff", function()
 	builtin.find_files()
 end, { noremap = true })
+
 map("n", "<leader>gf", function()
 	builtin.git_files()
 end, { noremap = true })
+
 map("n", "<leader>fb", function()
 	builtin.buffers()
 end, { noremap = true })
+
 map("n", "<leader>fh", function()
 	builtin.help_tags()
 end, { noremap = true })
 
 telescope.load_extension("fzf")
+telescope.load_extension("file_browser")
