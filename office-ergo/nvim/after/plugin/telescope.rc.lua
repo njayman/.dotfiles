a03bf8033f4c8989063365769ebabf24e3f479de
Najish Mahmud <najish@mirinfosys.com>
2023-01-11 16:44:18 +0600

    more new configs

--- office-ergo/nvim/after/plugin/telescope.rc.lua
+++ office-ergo/nvim/after/plugin/telescope.rc.lua
@@ -1,6 +1,6 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-	return
+    return
 end
 
 -- Setup Telescope
@@ -8,48 +8,49 @@ local builtin = require("telescope.builtin")
 local map = vim.keymap.set
 
 telescope.setup({
-	defaults = {
-		file_ignore_patterns = {
-			"build",
-			"dist",
-			"node_modules",
-		},
-	},
-	extensions = {
-		file_browser = {
-			hijack_netrw = true,
-		},
-		fzf = {
-			fuzzy = true, -- false will only do exact matching
-			override_generic_sorter = true, -- override the generic sorter
-			override_file_sorter = true, -- override the file sorter
-			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-			-- the default case_mode is "smart_case"
-		},
-	},
+    defaults = {
+        file_ignore_patterns = {
+            "build",
+            "dist",
+            "node_modules",
+        },
+    },
+    extensions = {
+        file_browser = {
+            hijack_netrw = true,
+        },
+        fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+        },
+    },
 })
 
 map("n", "<leader>fg", function()
-	builtin.live_grep()
+    builtin.live_grep()
 end, { noremap = true })
 
 map("n", "<leader>ff", function()
-	builtin.find_files()
+    builtin.find_files()
 end, { noremap = true })
 
 map("n", "<leader>gf", function()
-	builtin.git_files()
+    builtin.git_files()
 end, { noremap = true })
 
 map("n", "<leader>fb", function()
-	builtin.buffers()
+    builtin.buffers()
 end, { noremap = true })
 
 map("n", "<leader>fh", function()
-	builtin.help_tags()
+    builtin.help_tags()
 end, { noremap = true })
 
 map("n", "<leader>bf", ":Telescope file_browser<CR>", { noremap = true })
+map("n", "<leader>bc", ":Telescope file_browser path=%:p:h<CR>", { noremap = true })
 
 telescope.load_extension("fzf")
 telescope.load_extension("file_browser")
--- office-ergo/nvim/plugin/lspconfig.lua
+++ office-ergo/nvim/plugin/lspconfig.lua
@@ -1,92 +1,97 @@
 local status, nvim_lsp = pcall(require, "lspconfig")
 if not status then
-	return
+    return
 end
 
 local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 local enable_format_on_save = function(_, bufnr)
-	vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
-	vim.api.nvim_create_autocmd("BufWritePre", {
-		group = augroup_format,
-		buffer = bufnr,
-		callback = function()
-			vim.lsp.buf.format({ bufnr = bufnr })
-		end,
-	})
+    vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup_format,
+        buffer = bufnr,
+        callback = function()
+            vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+    })
 end
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function()
-	local opts = { noremap = true, silent = true, buffer = 0 }
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    local opts = { noremap = true, silent = true, buffer = 0 }
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-	vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gT", vim.lsp.buf.type_definition, opts)
 
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 
-	vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, opts)
-	vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, opts)
 
-	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
-	vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts)
 end
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 nvim_lsp.eslint.setup({
-	settings = {
-		codeActionsOnSave = {
-			enabled = true,
-			mode = "all",
-		},
-		run = "onSave",
-	},
+    settings = {
+        codeActionsOnSave = {
+            enabled = true,
+            mode = "all",
+        },
+        run = "onSave",
+    },
 })
 
 nvim_lsp.tsserver.setup({
-	on_attach = function(client, bufnr)
-		on_attach()
-		enable_format_on_save(client, bufnr)
-	end,
-	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-	cmd = { "typescript-language-server", "--stdio" },
-	capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        on_attach()
+        enable_format_on_save(client, bufnr)
+    end,
+    filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+    cmd = { "typescript-language-server", "--stdio" },
+    capabilities = capabilities,
 })
 
 nvim_lsp.sumneko_lua.setup({
-	capabilities = capabilities,
-	on_attach = function(client, bufnr)
-		on_attach()
-		enable_format_on_save(client, bufnr)
-	end,
-	settings = {
-		Lua = {
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { "vim" },
-			},
+    capabilities = capabilities,
+    on_attach = function(client, bufnr)
+        on_attach()
+        enable_format_on_save(client, bufnr)
+    end,
+    settings = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
 
-			workspace = {
-				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-		},
-	},
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+        },
+    },
 })
 
 nvim_lsp.pyright.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
 
 nvim_lsp.rust_analyzer.setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	-- Server-specific settings...
-	settings = {
-		["rust-analyzer"] = {},
-	},
+    on_attach = on_attach,
+    capabilities = capabilities,
+    -- Server-specific settings...
+    settings = {
+        ["rust-analyzer"] = {},
+    },
+})
+
+nvim_lsp.lemminx.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
 })
