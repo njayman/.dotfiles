1b3686633a82388fd4b188a6696f232b3bad73d0
najish-therap <najish.mahmud@therapservices.net>
2023-10-22 10:11:18 +0600

    added statusline and harpoon buffnav commands

--- nvim/.config/nvim/after/plugin/harpoon.rc.lua
+++ nvim/.config/nvim/after/plugin/harpoon.rc.lua
@@ -1,5 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>h", mark.add_file, { desc = "Mark files for harpoon" })
-vim.keymap.set("n", "<C-h>", ui.toggle_quick_menu, { desc = "Toggle harpoon ui" })
+vim.keymap.set("n", "<leader>hh", mark.add_file, { desc = "Harpoon: Mark files" })
+vim.keymap.set("n", "<leader>hu", ui.toggle_quick_menu, { desc = "Harpoon: Toggle ui" })
+vim.keymap.set("n", ",", ui.nav_prev, { desc = "Harpoon: Go to previous mark" })
+vim.keymap.set("n", ".", ui.nav_next, { desc = "Harpoon: Go to next mark" })
