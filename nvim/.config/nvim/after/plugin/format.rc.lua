d9f4f5564d1b8028a9fe2fdb7685880a52f6076f
njayman <najishm828282@gmail.com>
2023-11-03 11:16:08 +0600

    changed nvim js/ts formatter to prettierd

--- nvim/.config/nvim/after/plugin/format.rc.lua
+++ nvim/.config/nvim/after/plugin/format.rc.lua
@@ -3,12 +3,12 @@ local conform = require("conform")
 conform.setup({
     formatters_by_ft = {
         lua = { "stylua" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "prettier" },
-        html = { "prettier" },
-        json = { "prettier" },
+        javascript = { "prettierd" },
+        typescript = { "prettierd" },
+        javascriptreact = { "prettierd" },
+        typescriptreact = { "prettierd" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        json = { "prettierd" },
     }
 })
