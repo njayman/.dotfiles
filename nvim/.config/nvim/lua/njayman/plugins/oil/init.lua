be8fa8fd4ed4955a664c6950bd1b327fda2e373f
njayman <najishm828282@gmail.com>
2024-09-01 08:57:32 +0600

    fixed oil setup for sign column

--- nvim/.config/nvim/lua/njayman/plugins/oil/init.lua
+++ nvim/.config/nvim/lua/njayman/plugins/oil/init.lua
@@ -3,7 +3,12 @@ return {
 	opts = {},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
-		require("oil").setup({})
+		require("oil").setup({
+			delete_to_trash = true,
+			win_options = {
+				signcolumn = "yes",
+			},
+		})
 
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
